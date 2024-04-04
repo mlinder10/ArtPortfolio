6bdf293547f41d923ca989e2c6df6e2ca17b11a4
Matt Linder <linder2015@outlook.com>
2024-04-04 18:03:06 -0400

    upload

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -1,6 +1,7 @@
 import { GalleryItemType } from "../types";
 import { Dispatch, SetStateAction } from "react";
 import styles from "../styles/gallery.module.css";
+// import Reveal from "./Reveal";
 
 type GalleryItemProps = {
   item: GalleryItemType;
@@ -9,25 +10,30 @@ type GalleryItemProps = {
 
 export default function GalleryItem({ item, setItem }: GalleryItemProps) {
   return (
-    <div onClick={() => setItem(item.url)} className={styles["item-container"]}>
-      <img src={item.url} alt="" className={styles.img} />
-      <div className={styles["text-container"]}>
-        <p>
-          <b>{item.title}</b>
-        </p>
-        <p className={styles.healing}>Healing Power: {item.healingPower}</p>
-        <p className={styles.description}>{item.description}</p>
-        <p
-          className={styles.description}
-          style={{ color: item.descriptionTwoColor }}
-        >
-          <span style={{ textDecoration: "underline" }}>
-            Just one conscious breath
-          </span>
-          {item.descriptionTwo}
-        </p>
-        <p className={styles.description}>{item.descriptionThree}</p>
+    // <Reveal from="bottom">
+      <div
+        onClick={() => setItem(item.url)}
+        className={styles["item-container"]}
+      >
+        <img src={item.url} alt="" className={styles.img} />
+        <div className={styles["text-container"]}>
+          <p>
+            <b>{item.title}</b>
+          </p>
+          <p className={styles.healing}>Healing Power: {item.healingPower}</p>
+          <p className={styles.description}>{item.description}</p>
+          <p
+            className={styles.description}
+            style={{ color: item.descriptionTwoColor }}
+          >
+            <span style={{ textDecoration: "underline" }}>
+              Just one conscious breath
+            </span>
+            {item.descriptionTwo}
+          </p>
+          <p className={styles.description}>{item.descriptionThree}</p>
+        </div>
       </div>
-    </div>
+    // </Reveal>
   );
 }
