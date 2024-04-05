65a4a95b051ad6fe3014b1c72c36566a8467d601
Matt Linder <linder2015@outlook.com>
2024-04-05 12:00:53 -0400

    upload

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -11,29 +11,26 @@ type GalleryItemProps = {
 export default function GalleryItem({ item, setItem }: GalleryItemProps) {
   return (
     // <Reveal from="bottom">
-      <div
-        onClick={() => setItem(item.url)}
-        className={styles["item-container"]}
-      >
-        <img src={item.url} alt="" className={styles.img} />
-        <div className={styles["text-container"]}>
-          <p>
-            <b>{item.title}</b>
-          </p>
-          <p className={styles.healing}>Healing Power: {item.healingPower}</p>
-          <p className={styles.description}>{item.description}</p>
-          <p
-            className={styles.description}
-            style={{ color: item.descriptionTwoColor }}
-          >
-            <span style={{ textDecoration: "underline" }}>
-              Just one conscious breath
-            </span>
-            {item.descriptionTwo}
-          </p>
-          <p className={styles.description}>{item.descriptionThree}</p>
-        </div>
+    <div onClick={() => setItem(item.url)} className={styles["item-container"]}>
+      <img src={item.url} alt="" className={styles.img} />
+      <div className={styles["text-container"]}>
+        <p>
+          <b>{item.title}</b>
+        </p>
+        <p className={styles.healing}>Healing Power: {item.healingPower}</p>
+        <p className={styles.description}>{item.description}</p>
+        <p
+          className={styles.description}
+          style={{ color: item.descriptionTwoColor }}
+        >
+          <span style={{ textDecoration: "underline" }}>
+            <b>Just one conscious breath</b>
+          </span>
+          {item.descriptionTwo}
+        </p>
+        <p className={styles.description}>{item.descriptionThree}</p>
       </div>
+    </div>
     // </Reveal>
   );
 }
