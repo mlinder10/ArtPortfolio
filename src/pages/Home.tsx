20af666d9ee9ba287027e00574fbe6e124dd737a
Matt Linder <linder2015@outlook.com>
2024-04-18 11:00:43 -0400

    upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "../styles/home.module.css";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -8,7 +9,18 @@ export default function Home() {
       <p className={styles.title}>Welcome to the Creative Universe of Lynne</p>
       <p className={styles["title-two"]}>Sacred Geometry Designs</p>
       <p className={styles["title-three"]}>Medium: Love & Intention</p>
-      <p>Contact me at {"untilthecows@gmail.com"}</p>
+      <div className={styles.contact}>
+        <p>Contact me at {"untilthecows@gmail.com"}</p>
+        <a
+          href="https://www.facebook.com/share/WUokKi7ysdVmrP8c/?mibextid=LQQJ4d"
+          target="_blank"
+        >
+          <FaFacebook />
+        </a>
+        <a href="https://www.instagram.com/untilthecows" target="_blank">
+          <FaInstagram />
+        </a>
+      </div>
       <div className={styles["link-container"]}>
         <Link to="/gallery" className={styles.link}>
           View Work
